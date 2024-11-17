338bdadffa990deeb5ea20410331330ede7a58ca
popmanpop <zsasha957@gmail.com>
2024-11-17 15:58:13 +0300

    Создал динамичную генерацию и возможность редактировать новости

--- kyberteamSite/mainApp/views.py
+++ kyberteamSite/mainApp/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 
 def show_news_page(request):
-    return render(request, './index.html')
+    context = {
+        "firstNewCard": {
+            "tittle": "Идёт подготовка к турниру",
+            "imageURL": "https://cybersport.metaratings.ru/_images/insecure/w-680:h-512/bG9jYWw6Ly8vdXBsb2FkL3NwcmludC5lZGl0b3IvMWRiLzFkYmRlN2RiNzg5MmE5OWU4Mjg0NGZjNzc5YjFkNzdhLmpwZw==.webp",
+            "GameName": "DOTA 2",
+            "NewDate": "22.1111.2024",
+        },
+        "news_cards": [
+            {
+                "tittle": "Баба сеня обосрался, турнир отменяется",
+                "imageURL": "https://rg.ru/uploads/images/2023/10/05/kybersport_60c.jpg",
+                "gameName": "Standoff2",
+                "date": "1991.21.12"
+            },
+
+        ],
+    }
+    return render(request, './index.html', context)
 # Create your views here.
 def show_match_page(request):
     return render(request, './match_centre.html')
